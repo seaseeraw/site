5554589dd30a004948f4a9af3a0845e84048734c
seaseeraw <seaseeraw12@gmail.com>
2024-10-18 23:10:49 +1100

    simple check

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,30 @@
-import React from 'react'
+// import React from 'react'
 
-function About() {
+// function About() {
+//   return (
+//     <div className="let">About this company</div>
+//   )
+// }
+
+// export default About
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function BasicExample() {
   return (
-    <div className="let">About this company</div>
-  )
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
 }
 
-export default About
+export default BasicExample;
